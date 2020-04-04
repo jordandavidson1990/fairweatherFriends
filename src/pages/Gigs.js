9977059b5bfc3be3ddae20de25a697a1dcef5e3e
Jordan Davidson <jordandavidson1990@hotmail.com>
2020-04-04 15:42:56 +0100

    added css to link stream

--- src/pages/Gigs.js
+++ src/pages/Gigs.js
@@ -1,18 +1,20 @@
 import React from "react";
 import CountdownClock from "../components/Countdown";
+import "./Gigs.css";
 
 export default function Gigs() {
   const nextStream = new Date("2020-04-06T17:30:00.000Z");
   return (
-    <div>
+    <div className="gig-container">
       <h1>Gigs</h1>
       <p>Check facebook for live streams from Sam</p>
       <p></p>
-      <li>
-        <a href="https://www.facebook.com/FWFMusic">Live Stream</a>
-        <CountdownClock date={nextStream} content={"Next Set:"} />
-        <img src="assets/photos/Ross4.jpeg" alt="Sam and Jiggy" />
-      </li>
+      <a href="https://www.facebook.com/FWFMusic">
+        <li>Live Stream</li>
+      </a>
+
+      <CountdownClock date={nextStream} content={"Next Set:"} />
+      <img src="assets/photos/Ross4.jpeg" alt="Sam and Jiggy" />
     </div>
   );
 }
