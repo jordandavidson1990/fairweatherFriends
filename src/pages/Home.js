57ccfbae0b1779bf90b6bb0f9f2179dc19233fa8
Jordan Davidson <jordandavidson1990@hotmail.com>
2020-04-08 14:37:28 +0100

    removed keycode just for time being

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,27 +7,27 @@ export default function Home() {
   const [image, setImage] = useState("Jiggy_surreal.jpg");
   const [showBlindsidersButton, setShowBlindersidersButton] = useState(false);
   const [blindsidersBoolean, setBlindsidersBoolean] = useState(false);
-  const [keyCodeArray, setKeyCodeArray] = useState([])
-  const keyListen = (event) => {
-    // up, up, down, down, left, left, right, right
-    const cheatCodes = [38, 38, 40, 40, 37, 37, 39, 39];
-    const keycode = event.keyCode;
-    if (keycodeArray.length === 8) {
-      setKeycodeArray((prev)=> {...prev}).shift();
-    }
-    keycodeArray.push(keycode);
-    console.log("cheatCodes:", cheatCodes);
-    console.log("keyCodeArray:", keycodeArray);
-    for (let i = 0; i < cheatCodes.length; i++) {
-      if (cheatCodes[i] !== keycodeArray[i]) {
-        return false;
-      }
-    }
-    setShowBlindersidersButton(true);
+  const [keyCodeArray, setKeyCodeArray] = useState([]);
+  // const keyListen = (event) => {
+  //   // up, up, down, down, left, left, right, right
+  //   const cheatCodes = [38, 38, 40, 40, 37, 37, 39, 39];
+  //   const keycode = event.keyCode;
+  //   if (keycodeArray.length === 8) {
+  //     setKeycodeArray((prev)=> {...prev}).shift();
+  //   }
+  //   keycodeArray.push(keycode);
+  //   console.log("cheatCodes:", cheatCodes);
+  //   console.log("keyCodeArray:", keycodeArray);
+  //   for (let i = 0; i < cheatCodes.length; i++) {
+  //     if (cheatCodes[i] !== keycodeArray[i]) {
+  //       return false;
+  //     }
+  //   }
+  //   setShowBlindersidersButton(true);
 
-    // }
-  };
-  document.addEventListener("keyup", keyListen);
+  //   // }
+  // };
+  // document.addEventListener("keyup", keyListen);
 
   const showJazzyImage = () => {
     const boolean = !blindsidersBoolean;
